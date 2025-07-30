eafb5d49ae7e15d512bb7bf4bd3941b0ef90c2e9
isabelleadrury <isabelledrury38@gmail.com>
2025-07-30 12:56:32 -0400

    added prompt

--- froyo.js
+++ froyo.js
@@ -6,14 +6,22 @@ const flavors = [
   "coffee",
   "coffee",
 ];
-const frequencyCounter = {};
-for (let i = 0; i < flavors.length; i++) {
-  const flavor = flavors[i];
-  console.log("flavor", flavor);
-  if (flavor in frequencyCounter) {
-    frequencyCounter[flavor] += 1;
-  } else {
-    frequencyCounter[flavor] = 1;
-  }
+// prompt("Froyo Flavors");
+// const frequencyCounter = {};
+// for (let i = 0; i < flavors.length; i++) {
+//   const flavor = flavors[i];
+//   console.log("flavor", flavor);
+//   if (flavor in frequencyCounter) {
+//     frequencyCounter[flavor] += 1;
+//   } else {
+//     frequencyCounter[flavor] = 1;
+//   }
+// }
+
+let flavor = prompt("Please order your flavors:");
+
+if (flavors !== null) {
+  console.log("Your order is " + flavors + "!");
+} else {
+  console.log("You cancelled the prompt.");
 }
-prompt(flavors);
