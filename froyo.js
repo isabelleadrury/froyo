ee323109b52f32049071265f4a8abe55c55b398f
isabelleadrury <isabelledrury38@gmail.com>
2025-07-30 13:21:46 -0400

    froyo flavors separated by commas

--- froyo.js
+++ froyo.js
@@ -7,11 +7,23 @@ const flavors = [
   "coffee",
 ];
 
-let flavorFroyo = prompt("Please order your flavors:");
-console.log(flavor["froyo"]);
+let flavor = prompt("Please order your flavors:");
+console.log(flavor);
 
 if (flavor !== null) {
   console.log("Your order is " + flavor + "!");
 } else {
   console.log("You cancelled the prompt.");
 }
+
+const frequencyCounter = {};
+for (let i = 0; i < flavors.length; i++) {
+  const flavor = flavors[i];
+  console.log("flavors", flavor);
+  if (flavor in frequencyCounter) {
+    frequencyCounter[flavor] += 1;
+  } else {
+    frequencyCounter[flavor] = 1;
+  }
+}
+console.log("frequency", frequencyCounter);
